2d4f1f44666e88b1e1c0e436849a5dbf47eb99f1
Pale-Champion-Atom596 <adamhoffensetz@gmail.com>
2023-03-03 21:06:10 -0500

    'Zoomable' Grid Map
    Using the scroll wheel changes the map scale. Updated to version Ð4.

--- main.py
+++ main.py
@@ -7,36 +7,42 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (128, 128, 128)
 
-# Tile and Grid Size
-TILE_DIM = 16
-GRID_WIDTH = 32
-GRID_HEIGHT = 32
+
+class Properties:
+    # Tile and Grid Size
+    TILE_DIM = 16
+    GRID_WIDTH = 32
+    GRID_HEIGHT = 32
 
 
 # Single Tile Drawing
 def draw_tile(screen, x, y, color, offset_x, offset_y):
     # calculate the position of the top left corner of the tile
-    tile_x = (x - y) * TILE_DIM + offset_x
-    tile_y = (x + y) * TILE_DIM / 2 + offset_y
+    tile_x = (x - y) * Properties.TILE_DIM + offset_x
+    tile_y = (x + y) * Properties.TILE_DIM / 2 + offset_y
     # calculate the position of the center of the tile
-    center_x = tile_x + TILE_DIM
-    center_y = tile_y + TILE_DIM / 2
+    center_x = tile_x + Properties.TILE_DIM
+    center_y = tile_y + Properties.TILE_DIM / 2
     # draw the tile as a polygon
     pygame.draw.polygon(screen, color, [
         (center_x, tile_y),
-        (tile_x + TILE_DIM * 2, center_y),
-        (center_x, tile_y + TILE_DIM),
+        (tile_x + Properties.TILE_DIM * 2, center_y),
+        (center_x, tile_y + Properties.TILE_DIM),
         (tile_x, center_y)
     ])
 
 
 # Grid Drawing
 def draw_grid(screen, o_x, o_y):
-    for y in range(GRID_HEIGHT):
-        for x in range(GRID_WIDTH):
+    for y in range(Properties.GRID_HEIGHT):
+        for x in range(Properties.GRID_WIDTH):
             # alternate the color of the tiles
             color = WHITE if (x + y) % 2 == 0 else GREY
             draw_tile(screen, x, y, color, o_x, o_y)
+
+
+def draw_gui(screen):
+    print("Not yet implemented")
 
 
 def init(properties):
@@ -47,8 +53,9 @@ def init(properties):
 
     running = True
     panning = False
-    o_x = pygame.display.get_surface().get_size()[0] / 2
-    o_y = (pygame.display.get_surface().get_size()[1] - TILE_DIM * GRID_WIDTH) / 2
+    w, h = pygame.display.get_surface().get_size()
+    o_x = w / 2
+    o_y = (h - Properties.TILE_DIM * Properties.GRID_WIDTH) / 2
     o_tx, o_ty = 0, 0
     while running:
         for event in pygame.event.get():
@@ -68,12 +75,21 @@ def init(properties):
                 o_y += (event.pos[1] - o_ty)
                 o_tx, o_ty = event.pos
 
-            elif event.type == pygame.WINDOWRESIZED:
-                o_x = pygame.display.get_surface().get_size()[0] / 2
-                o_y = (pygame.display.get_surface().get_size()[1] - TILE_DIM * GRID_WIDTH) / 2
+            elif event.type == pygame.MOUSEWHEEL:
+                Properties.TILE_DIM += event.y * 2
+                if Properties.TILE_DIM <= 2:
+                    Properties.TILE_DIM = 2
+
+            elif event.type == pygame.WINDOWRESIZED:  # Game Window size changed
+                nw, nh = pygame.display.get_surface().get_size()
+                o_x *= nw / w
+                o_y = (nh - Properties.TILE_DIM * Properties.GRID_WIDTH) / 2
+                # o_y = nh - (o_y + TILE_DIM * GRID_WIDTH / 2) * nh / h
+                w, h = nw, nh
 
         screen.fill(BLACK)
         draw_grid(screen, o_x, o_y)
+        # draw_gui(screen)
         pygame.display.update()
 
 
